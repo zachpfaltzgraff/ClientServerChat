0a50c566264ddddfb93460c05b7afd2df134846b
zachpfaltzgraff <zachpfaltzgraff@gmail.com>
2024-02-09 09:26:51 -0500

    server created

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,6 +1,35 @@
-public class ChatServer {
-    public static void main(String[] args) {
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class ChatServer {
+    static int PORT;
+    public static void main(String[] args) {
+        PORT = 12345;
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Server Started. Waiting for clients...");
+
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected: "+ clientSocket.getInetAddress());
+
+            while(true) {
+                Scanner input = new Scanner(clientSocket.getInputStream());
+                Scanner serverInput = new Scanner(System.in);
+                PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                String messageIn = input.nextLine();
+                System.out.println(clientSocket.getInetAddress() + ": " + messageIn);
+
+                String messageOut = serverInput.nextLine();
+                System.out.println(serverSocket.getInetAddress() + ": " + messageOut);
+                output.println(serverSocket.getInetAddress() + ": " + messageOut);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
